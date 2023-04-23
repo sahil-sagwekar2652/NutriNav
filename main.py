a711096117eb35d323dbf01e70eb2bb10489a3f8
sahil_sagwekar <sahilss2652@gmail.com>
2023-04-23 15:15:10 +0530

    added recipe and feed me page

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
+import requests
 import json
 from os import environ as env
 from urllib.parse import quote_plus, urlencode
 
 from authlib.integrations.flask_client import OAuth
 from dotenv import find_dotenv, load_dotenv
-from flask import Flask, redirect, render_template, session, url_for
+from flask import Flask, redirect, render_template, session, url_for, request
 
 ENV_FILE = find_dotenv()
 if ENV_FILE:
@@ -31,8 +32,7 @@ oauth.register(
 def home():
     return render_template(
         "index.html",
-        session=session.get("user"),
-        pretty=json.dumps(session.get("user"), indent=4),
+        current_user=session.get("user")
     )
 
 
@@ -65,6 +65,88 @@ def logout():
             quote_via=quote_plus,
         )
     )
+
+
+@app.route("/feed_me", methods=['GET', 'POST'])
+def feed_me():
+    current_user = session.get('user')
+    if current_user:
+
+        if request.method == 'POST':
+            loc = request.form['location']
+            if "$" in loc:
+                lat = loc.split("$")[0]
+                lon = loc.split("$")[1]
+                response = requests.get(
+                    f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={env.get('OW_API_KEY')}"
+                )
+                weather_data = response.json()
+            else:
+                # Fetching latitude and logitude from geocoding api
+                geo_response = requests.get(
+                    f"http://api.openweathermap.org/geo/1.0/direct?q={loc}&limit=1&appid={env.get('OW_API_KEY')}"
+                )
+                data = geo_response.json()
+                lat = data[0]["lat"]
+                lon = data[0]["lon"]
+                response = requests.get(
+                    f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={env.get('OW_API_KEY')}"
+                )
+                weather_data = response.json()
+            # extracting relevant data from the openweather api response
+            temperature = weather_data["main"]["temp"]
+            description = weather_data["weather"][0]["description"]
+
+            # Suggest ingredients based on the current weather conditions
+            if temperature < 10:
+                suggested_ingredients = ["potatoes", "carrots", "onions", "beef"]
+            elif temperature < 20:
+                suggested_ingredients = ["chicken", "rice", "beans", "tomatoes", 'salads']
+            else:
+                suggested_ingredients = ["lettuce", "spinach", "cucumber", "avocado"]
+
+            # print(suggested_ingredients, "\n", description, "\n\n\n", weather_data)
+            # print(",".join(suggested_ingredients))
+
+            recipe_response = requests.get(
+                'https://api.edamam.com/api/recipes/v2',
+                params={
+                    'type': "public",
+                    'q': ",".join(suggested_ingredients),
+                    'app_id': env.get('RECIPE_APP_ID'),
+                    'app_key': env.get('RECIPE_APP_KEY'),
+                    'random': "true"
+                }
+            )
+            recipe_data = recipe_response.json()
+            print("\n\n", recipe_data["hits"][0])
+            uri = recipe_data["hits"][0]['recipe']['uri']
+            img = recipe_data["hits"][0]['recipe']['image']
+            label = recipe_data["hits"][0]['recipe']['label']
+            ingredients = recipe_data["hits"][0]['recipe']['ingredientLines']
+
+            return render_template('recipe.html', ingredients=ingredients, img=img, label=label)
+        else:
+            return render_template(
+                "feed_me.html",
+                current_user=session.get('user')
+            )
+    else:
+        return redirect("/login")
+
+
+@app.route('/recipe')
+def recipe():
+    current_user = session.get('user')
+    if current_user:
+        return render_template("recipe.html", uri=uri, )
+    else:
+        return redirect("/login")
+
+
+@app.errorhandler(404)
+def not_found_error(error):
+    return render_template('404.html'), 404
 
 
 if __name__ == "__main__":
--- static/js/feed_me.js
+++ static/js/feed_me.js
@@ -1,17 +1,15 @@
-
-
-function getLocation(location) {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(showPosition);
-    } else {
-      alert("Geolocation is not supported by this browser.");
-    }
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else {
+    alert("Geolocation is not supported by this browser.");
   }
+}
 
-  function showPosition(position) {
-    var latitude = position.coords.latitude;
-    var longitude = position.coords.longitude;
-    document.getElementById("location").value = latitude + ", " + longitude;
-  }
-    
-  console.log(showPosition());
+function showPosition(position) {
+  var latitude = position.coords.latitude;
+  var longitude = position.coords.longitude;
+  var locationInput = document.getElementById("location");
+  locationInput.value = latitude + "$" + longitude;
+  document.forms[0].submit();
+}
