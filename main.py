106f3c23e95dc44739980a25c071dfa3a8ec5aba
sahil_sagwekar <sahilss2652@gmail.com>
2023-04-23 15:45:55 +0530

    fixed callback uri

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ def callback():
 @app.route("/login")
 def login():
     return oauth.auth0.authorize_redirect(
-        redirect_uri=url_for("/callback", _external=True)
+        redirect_uri=url_for("callback", _external=True)
     )
 
 
